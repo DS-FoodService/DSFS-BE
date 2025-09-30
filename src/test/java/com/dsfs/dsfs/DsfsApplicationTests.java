cb778c73f037ef5d4a5d56d31d5009490c4b7def
20210699 <20210699@duksung.ac.kr>
2025-09-30 19:16:35 +0900

    [FEAT] init

---
+++ src/test/java/com/dsfs/dsfs/DsfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsfs.dsfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
