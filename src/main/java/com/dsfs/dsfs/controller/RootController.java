a8c92b4853a78c9a49415a77112e61108f453d69
20210699 <20210699@duksung.ac.kr>
2025-12-18 00:25:27 +0900

    [Feat] RootController

---
+++ src/main/java/com/dsfs/dsfs/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.dsfs.dsfs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
